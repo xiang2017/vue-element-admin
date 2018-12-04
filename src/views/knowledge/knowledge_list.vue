d7cb43ffb4e16ed9049e161fcccc6c6f00882370
xiang <yuxiangxiang@sanjieke.com>
2018-12-04 18:43:06 +0800

    知识新增筛选，分页

--- src/views/knowledge/knowledge_list.vue
+++ src/views/knowledge/knowledge_list.vue
@@ -1,12 +1,27 @@
 <template>
   <div class="app-container">
-    <el-button type="primary" size="small">
-      <router-link to="/content/knowledge/edit">新增知识点</router-link>
-    </el-button>
 
-    <el-table :data="knowledge" border style="width: 100%; margin-top: 10px">
+    <div>
+      <el-select v-model="params.label_id" placeholder="请选择标签">
+        <el-option
+          v-for="item in labels"
+          :key="item.id"
+          :label="item.name"
+          :value="item.id">
+        </el-option>
+      </el-select>
+
+      <el-button @click="getPageData(0)">搜索</el-button>
+
+      <el-button type="primary" style="margin-left: 10px">
+        <router-link to="/content/knowledge/edit">新增知识点</router-link>
+      </el-button>
+    </div>
+
+    <el-table :data="knowledge" border style="width: 100%; margin: 10px 0">
       <el-table-column prop="id" label="ID"></el-table-column>
       <el-table-column prop="name" label="Name"></el-table-column>
+      <el-table-column prop="label_name" label="标签名"></el-table-column>
       <el-table-column label="操作">
         <template slot-scope="know">
           <router-link v-bind:to="'/content/knowledge/edit/' + know.row.id">
@@ -15,9 +30,16 @@
         </template>
       </el-table-column>
     </el-table>
+
+    <el-pagination
+      layout="prev, pager, next"
+      @current-change="whenPageChose"
+      :total="count">
+    </el-pagination>
   </div>
 </template>
 <script>
+import { fetchList as labelList } from '@/api/label'
 import { fetchList } from '@/api/knowledge'
 export default {
   name: 'Knowledge',
@@ -27,25 +49,44 @@ export default {
       count: 0,
       params: {
         page: 0,
-      }
+        label_id: ''
+      },
+      labels: [],
     }
   },
   methods: {
+    whenPageChose(e){
+      this.getPageData(e - 1);
+    },
     getPageData(page) {
       this.params.page = page;
       fetchList(this.params).then(res => {
-        this.knowledge = res.data.data
-        this.count = res.data.count
+        this.knowledge = res.data.data;
+        this.count = res.data.count;
       }).catch(error => {
         if (error.response) {
           error = error.response.data
         }
         this.$message.error(error)
       })
+    },
+    getLabels (){
+      labelList({page: 0}).then(response => {
+        this.labels = [{
+          "id": 0,
+          "name": "所有标签"
+        }];
+        response.data.forEach(e => {
+          this.labels.push(e)
+        });
+      }).catch(error => {
+          console.error(error)
+      })
     }
   },
   mounted() {
     this.getPageData(0)
+    this.getLabels();
   }
 }
 </script>
